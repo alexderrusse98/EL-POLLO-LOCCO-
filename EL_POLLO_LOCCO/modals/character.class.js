4b94cb6a038bce3de5c1028c9304667b123a98c0
alexderrusse98 <alex.weiswasser.com@gmail.com>
2025-09-30 12:57:01 +0200

    gravity

--- EL_POLLO_LOCCO/modals/character.class.js
+++ EL_POLLO_LOCCO/modals/character.class.js
@@ -1,9 +1,9 @@
 class Character extends MovableObject {
-   height = 250;
-   y = 180;
-   speed = 5;
+    height = 250;
+    y = 80;
+    speed = 5;
 
-IMAGES_WALKING = [
+    IMAGES_WALKING = [
         './img/img_pollo_locco/img/2_character_pepe/2_walk/W-21.png',
         './img/img_pollo_locco/img/2_character_pepe/2_walk/W-22.png',
         './img/img_pollo_locco/img/2_character_pepe/2_walk/W-23.png',
@@ -11,42 +11,42 @@ IMAGES_WALKING = [
         './img/img_pollo_locco/img/2_character_pepe/2_walk/W-25.png',
         './img/img_pollo_locco/img/2_character_pepe/2_walk/W-26.png',
     ];
-world;
+    world;
 
 
 
-constructor(){
-    super().loadImage('./img/img_pollo_locco/img/2_character_pepe/2_walk/W-21.png');
-    this.loadImages(this.IMAGES_WALKING);
-
-    this.animate();
-}
-
-animate(){
-
-     setInterval(() => {
-         if (this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x) {
-            this.x += this.speed;
-            this.ortherDirection = false;
-        }  
-
-        if (this.world.keyboard.LEFT && this.x >= 0) {
-            this.x -= this.speed;
-            this.ortherDirection = true;
-        }
-        this.world.camera_x = -this.x + 100;
-    }, 1000 / 60);
-
-
-    setInterval(() => {
-        if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
-            this.playAnimation(this.IMAGES_WALKING);
+    constructor() {
+        super().loadImage('./img/img_pollo_locco/img/2_character_pepe/2_walk/W-21.png');
+        this.loadImages(this.IMAGES_WALKING);
+        this.applyGravity();
+        this.animate();
     }
-    }, 50);
-    
-}
 
-    jump(){
+    animate() {
+
+        setInterval(() => {
+            if (this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x) {
+                this.x += this.speed;
+                this.ortherDirection = false;
+            }
+
+            if (this.world.keyboard.LEFT && this.x >= 0) {
+                this.x -= this.speed;
+                this.ortherDirection = true;
+            }
+            this.world.camera_x = -this.x + 100;
+        }, 1000 / 60);
+
+
+        setInterval(() => {
+            if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
+                this.playAnimation(this.IMAGES_WALKING);
+            }
+        }, 50);
+
+    }
+
+    jump() {
 
     }
 }
--- EL_POLLO_LOCCO/modals/moveable_object.class.js
+++ EL_POLLO_LOCCO/modals/moveable_object.class.js
@@ -1,21 +1,32 @@
- class MovableObject{
-    x = 120;
-    y = 340;
-    img;
-    height = 100;
-    width = 100;
-    imageCache = {};
-    currentImage = 0; 
-    speed = 0.15; 
-    ortherDirection = false;
+class MovableObject {
+   x = 120;
+   y = 340;
+   img;
+   height = 100;
+   width = 100;
+   imageCache = {};
+   currentImage = 0;
+   speed = 0.15;
+   ortherDirection = false;
+   speedY = 0;
+   acceleration = 1;
 
 
-   loadImage(path){
+   applyGravity() {
+      setInterval(() => {
+         if (this.y < 180) {
+            this.y -= this.speedY;
+            this.speedY -= this.acceleration;
+         }
+      }, 1000 / 25)
+   }
+
+   loadImage(path) {
       this.img = new Image();
       this.img.src = path;
    }
 
-   loadImages(arr){
+   loadImages(arr) {
       arr.forEach((path) => {
          let img = new Image();
          img.src = path
@@ -23,25 +34,25 @@
       });
    }
 
-   playAnimation(images){
-            let i = this.currentImage % this.IMAGES_WALKING.length;
-            let path = images[i];
-            this.img = this.imageCache[path];
-            this.currentImage++;
+   playAnimation(images) {
+      let i = this.currentImage % this.IMAGES_WALKING.length;
+      let path = images[i];
+      this.img = this.imageCache[path];
+      this.currentImage++;
    }
 
-     moveRight(){
+   moveRight() {
 
-    }
+   }
 
-      moveLeft() {
-        setInterval(() => {
-            this.x -= this.speed;
-          
-            if (this.x + this.width < 0) {
-                this.x = 1000 + Math.random() * 500;
-            }
-        }, 1000 / 60);
-    }
+   moveLeft() {
+      setInterval(() => {
+         this.x -= this.speed;
 
- }
+         if (this.x + this.width < 0) {
+            this.x = 1000 + Math.random() * 500;
+         }
+      }, 1000 / 60);
+   }
+
+}
