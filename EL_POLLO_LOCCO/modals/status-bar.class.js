fd289d85f6e8abfd5c5eba6411acaac014ec454f
alexderrusse98 <alex.weiswasser.com@gmail.com>
2025-10-29 14:30:36 +0100

    Statusbars_img

--- EL_POLLO_LOCCO/modals/status-bar.class.js
+++ EL_POLLO_LOCCO/modals/status-bar.class.js
@@ -1,4 +1,14 @@
 class StatusBar extends DrawableObject {
+    IMAGES_COIN = [
+        './img/img_pollo_locco/img/7_statusbars/1_statusbar/1_statusbar_coin/blue/0.png',
+        './img/img_pollo_locco/img/7_statusbars/1_statusbar/1_statusbar_coin/blue/20.png',
+        './img/img_pollo_locco/img/7_statusbars/1_statusbar/1_statusbar_coin/blue/40.png',
+        './img/img_pollo_locco/img/7_statusbars/1_statusbar/1_statusbar_coin/blue/60.png',
+        './img/img_pollo_locco/img/7_statusbars/1_statusbar/1_statusbar_coin/blue/80.png',
+        './img/img_pollo_locco/img/7_statusbars/1_statusbar/1_statusbar_coin/blue/100.png',
+    ]
+    
+    
     IMAGES = [
         './img/img_pollo_locco/img/7_statusbars/1_statusbar/2_statusbar_health/blue/0.png',
         './img/img_pollo_locco/img/7_statusbars/1_statusbar/2_statusbar_health/blue/20.png',
@@ -7,7 +17,16 @@ class StatusBar extends DrawableObject {
         './img/img_pollo_locco/img/7_statusbars/1_statusbar/2_statusbar_health/blue/80.png',
         './img/img_pollo_locco/img/7_statusbars/1_statusbar/2_statusbar_health/blue/100.png'
     ];
-
+  
+    
+    IMAGES_BOTTLE = [
+        './img/img_pollo_locco/img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/0.png',
+        './img/img_pollo_locco/img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/20.png',
+        './img/img_pollo_locco/img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/40.png',
+        './img/img_pollo_locco/img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/60.png',
+        './img/img_pollo_locco/img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/80.png',
+        './img/img_pollo_locco/img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/100.png',
+    ];
     percentage = 100;
 
 
