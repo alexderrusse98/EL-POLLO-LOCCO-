aea44a65bd03d6869b4bc5b7c3f43067d171546c
alexderrusse98 <alex.weiswasser.com@gmail.com>
2025-11-05 21:41:00 +0100

    smoother rotation

--- EL_POLLO_LOCCO/modals/throwable-object.class.js
+++ EL_POLLO_LOCCO/modals/throwable-object.class.js
@@ -1,7 +1,6 @@
 class ThrowableObject extends MovableObject {
     
     IMAGES_ROTATION = [
-        './img/img_pollo_locco/img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png',
         './img/img_pollo_locco/img/6_salsa_bottle/bottle_rotation/2_bottle_rotation.png',
         './img/img_pollo_locco/img/6_salsa_bottle/bottle_rotation/3_bottle_rotation.png',
         './img/img_pollo_locco/img/6_salsa_bottle/bottle_rotation/4_bottle_rotation.png',
