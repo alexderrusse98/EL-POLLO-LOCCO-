cd63b52f148806b592eca5b4c140d6955c63ca71
alexderrusse98 <alex.weiswasser.com@gmail.com>
2025-11-09 12:28:48 +0100

    add_idle_images

--- EL_POLLO_LOCCO/modals/character.class.js
+++ EL_POLLO_LOCCO/modals/character.class.js
@@ -44,6 +44,32 @@ class Character extends MovableObject {
         './img/img_pollo_locco/img/2_character_pepe/4_hurt/H-43.png'
     ];
 
+    IMAGES_IDLE = [
+        './img/img_pollo_locco/img/2_character_pepe/1_idle/idle/I-1.png',
+        './img/img_pollo_locco/img/2_character_pepe/1_idle/idle/I-2.png',
+        './img/img_pollo_locco/img/2_character_pepe/1_idle/idle/I-3.png',
+        './img/img_pollo_locco/img/2_character_pepe/1_idle/idle/I-4.png',
+        './img/img_pollo_locco/img/2_character_pepe/1_idle/idle/I-5.png',
+        './img/img_pollo_locco/img/2_character_pepe/1_idle/idle/I-6.png',
+        './img/img_pollo_locco/img/2_character_pepe/1_idle/idle/I-7.png',
+        './img/img_pollo_locco/img/2_character_pepe/1_idle/idle/I-8.png',
+        './img/img_pollo_locco/img/2_character_pepe/1_idle/idle/I-9.png',
+        './img/img_pollo_locco/img/2_character_pepe/1_idle/idle/I-10.png'
+    ];
+
+    IMAGES_LONGIDLE = [
+        './img/img_pollo_locco/img/2_character_pepe/1_idle/long_idle/I-11.png',
+        './img/img_pollo_locco/img/2_character_pepe/1_idle/long_idle/I-12.png',
+        './img/img_pollo_locco/img/2_character_pepe/1_idle/long_idle/I-13.png',
+        './img/img_pollo_locco/img/2_character_pepe/1_idle/long_idle/I-14.png',
+        './img/img_pollo_locco/img/2_character_pepe/1_idle/long_idle/I-15.png',
+        './img/img_pollo_locco/img/2_character_pepe/1_idle/long_idle/I-16.png',
+        './img/img_pollo_locco/img/2_character_pepe/1_idle/long_idle/I-17.png',
+        './img/img_pollo_locco/img/2_character_pepe/1_idle/long_idle/I-18.png',
+        './img/img_pollo_locco/img/2_character_pepe/1_idle/long_idle/I-19.png',
+        './img/img_pollo_locco/img/2_character_pepe/1_idle/long_idle/I-20.png'
+    ];
+
     world;
 
 
