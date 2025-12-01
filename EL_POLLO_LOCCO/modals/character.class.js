c4c9a86df8eb0fe8355a99ee0e917e67a934978b
alexderrusse98 <alex.weiswasser.com@gmail.com>
2025-12-01 21:37:38 +0100

    add imges

--- EL_POLLO_LOCCO/modals/character.class.js
+++ EL_POLLO_LOCCO/modals/character.class.js
@@ -34,12 +34,68 @@ class Character extends MovableObject {
     lastMoveTime = new Date().getTime();
 
     /** Arrays of image paths for character animations */
-    IMAGES_WALKING = [ /* walking frames */];
-    IMAGES_JUMPING = [ /* jumping frames */];
-    IMAGES_DEAD = [ /* death frames */];
-    IMAGES_HURT = [ /* hurt frames */];
-    IMAGES_IDLE = [ /* idle frames */];
-    IMAGES_LONGIDLE = [ /* long idle frames */];
+    IMAGES_WALKING = [
+        './img/img_pollo_locco/img/2_character_pepe/2_walk/W-21.png',
+        './img/img_pollo_locco/img/2_character_pepe/2_walk/W-22.png',
+        './img/img_pollo_locco/img/2_character_pepe/2_walk/W-23.png',
+        './img/img_pollo_locco/img/2_character_pepe/2_walk/W-24.png',
+        './img/img_pollo_locco/img/2_character_pepe/2_walk/W-25.png',
+        './img/img_pollo_locco/img/2_character_pepe/2_walk/W-26.png',
+    ];
+
+    IMAGES_JUMPING = [
+        './img/img_pollo_locco/img/2_character_pepe/3_jump/J-31.png',
+        './img/img_pollo_locco/img/2_character_pepe/3_jump/J-32.png',
+        './img/img_pollo_locco/img/2_character_pepe/3_jump/J-33.png',
+        './img/img_pollo_locco/img/2_character_pepe/3_jump/J-34.png',
+        './img/img_pollo_locco/img/2_character_pepe/3_jump/J-35.png',
+        './img/img_pollo_locco/img/2_character_pepe/3_jump/J-36.png',
+        './img/img_pollo_locco/img/2_character_pepe/3_jump/J-37.png',
+        './img/img_pollo_locco/img/2_character_pepe/3_jump/J-38.png',
+        './img/img_pollo_locco/img/2_character_pepe/3_jump/J-39.png',
+    ];
+    
+    IMAGES_DEAD = [
+        './img/img_pollo_locco/img/2_character_pepe/5_dead/D-51.png',
+        './img/img_pollo_locco/img/2_character_pepe/5_dead/D-52.png',
+        './img/img_pollo_locco/img/2_character_pepe/5_dead/D-53.png',
+        './img/img_pollo_locco/img/2_character_pepe/5_dead/D-54.png',
+        './img/img_pollo_locco/img/2_character_pepe/5_dead/D-55.png',
+        './img/img_pollo_locco/img/2_character_pepe/5_dead/D-56.png',
+        './img/img_pollo_locco/img/2_character_pepe/5_dead/D-57.png'
+    ];
+
+    IMAGES_HURT = [
+        './img/img_pollo_locco/img/2_character_pepe/4_hurt/H-41.png',
+        './img/img_pollo_locco/img/2_character_pepe/4_hurt/H-42.png',
+        './img/img_pollo_locco/img/2_character_pepe/4_hurt/H-43.png'
+    ];
+
+    IMAGES_IDLE = [
+        './img/img_pollo_locco/img/2_character_pepe/1_idle/idle/I-1.png',
+        './img/img_pollo_locco/img/2_character_pepe/1_idle/idle/I-2.png',
+        './img/img_pollo_locco/img/2_character_pepe/1_idle/idle/I-3.png',
+        './img/img_pollo_locco/img/2_character_pepe/1_idle/idle/I-4.png',
+        './img/img_pollo_locco/img/2_character_pepe/1_idle/idle/I-5.png',
+        './img/img_pollo_locco/img/2_character_pepe/1_idle/idle/I-6.png',
+        './img/img_pollo_locco/img/2_character_pepe/1_idle/idle/I-7.png',
+        './img/img_pollo_locco/img/2_character_pepe/1_idle/idle/I-8.png',
+        './img/img_pollo_locco/img/2_character_pepe/1_idle/idle/I-9.png',
+        './img/img_pollo_locco/img/2_character_pepe/1_idle/idle/I-10.png'
+    ];
+
+    IMAGES_LONGIDLE = [
+        './img/img_pollo_locco/img/2_character_pepe/1_idle/long_idle/I-11.png',
+        './img/img_pollo_locco/img/2_character_pepe/1_idle/long_idle/I-12.png',
+        './img/img_pollo_locco/img/2_character_pepe/1_idle/long_idle/I-13.png',
+        './img/img_pollo_locco/img/2_character_pepe/1_idle/long_idle/I-14.png',
+        './img/img_pollo_locco/img/2_character_pepe/1_idle/long_idle/I-15.png',
+        './img/img_pollo_locco/img/2_character_pepe/1_idle/long_idle/I-16.png',
+        './img/img_pollo_locco/img/2_character_pepe/1_idle/long_idle/I-17.png',
+        './img/img_pollo_locco/img/2_character_pepe/1_idle/long_idle/I-18.png',
+        './img/img_pollo_locco/img/2_character_pepe/1_idle/long_idle/I-19.png',
+        './img/img_pollo_locco/img/2_character_pepe/1_idle/long_idle/I-20.png'
+    ];
 
     /** Reference to the game world */
     world;
